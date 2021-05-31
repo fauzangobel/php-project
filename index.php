6c1098604a01e6f029504a955a06c1f5f5f387f8
Andi Moh Soreang <85069651+andimohsoreang@users.noreply.github.com>
2021-05-31 21:11:36 +0800

    Mengubah File Index.html
    - Menambahkan kata Landing Page Pada page landing page. fungsi : agar user bisa tahu bahwa page yang dibuka adalah page awala atau landing page.
    - Menambahakan Copyrights dan Author
    - Menambahkan Keterangan " Check What U Want on list above"!

--- index.php
+++ index.php
@@ -8,12 +8,14 @@
 </head>
 <body>
     
-    <h1>Welcome To My Hotel</h1>
+    <h1>Welcome To My Hotel - Landing Page</h1>
 
     <h3>We Ready To serve you better :)</h3>
+    <h4> Check What you Want on list above ! </h4>
     <ul>
         <li> <a href="form_orders.php">Get Your Room !</a> </li>
     </ul>
 
+    <p> Copyrights @ My-Hotel - Author : Andi Mohamad Nurholis Sorejang </p>
 </body>
 </html>
